66a28f97fa41bee2af2e7f3d047d93e4efa60adb
Alperen Dagli <alperen.dagli43@gmail.com>
2023-11-17 22:18:28 +0300

    Revert "ai generated changes cycle 1"
    This reverts commit a5a9372eb01355a351df00524f00751a7d87664c.

--- src/main/java/com/CarRentalAgency/entity/Car.java
+++ src/main/java/com/CarRentalAgency/entity/Car.java
@@ -1,5 +1,6 @@
 package com.CarRentalAgency.entity;
 
+
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
@@ -15,63 +16,63 @@ import org.hibernate.validator.constraints.Length;
 @NoArgsConstructor
 public class Car {
 
-@Id
-@SequenceGenerator(
-name = "car_seq",
-sequenceName = "car_seq",
-allocationSize = 1,
-initialValue = 18 // start from 18 because we have 17 cars in the database.
-)
-@GeneratedValue(
-strategy = GenerationType.SEQUENCE,
-generator = "car_seq")
-private Long id;
+    @Id
+    @SequenceGenerator(
+            name = "car_seq",
+            sequenceName = "car_seq",
+            allocationSize = 1,
+            initialValue = 18 // start from 18 because we have 17 cars in the database.
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "car_seq")
+    private Long id;
 
 
-@Length(min = 5, max = 50)
-private String name;
+    @Length(min = 5, max = 50)
+    private String name;
 
-@Column(
-unique = true,
-nullable = false
-)
-private int registrationNumber;
+    @Column(
+            unique = true,
+            nullable = false
+    )
+    private int registrationNumber;
 
-@NotNull
-private short seats;
+    @NotNull
+    private short seats;
 
-@NotNull
-private int kilometres;
+    @NotNull
+    private int kilometres;
 
-@NotNull
-private short doors;
+    @NotNull
+    private short doors;
 
-@Enumerated(EnumType.STRING)
-@Column(name = "model")
-private Model model;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "model")
+    private Model model;
 
-@Enumerated(EnumType.STRING)
-private Gear gear;
+    @Enumerated(EnumType.STRING)
+    private Gear gear;
 
-@Enumerated(EnumType.STRING)
-@Column(name = "fuel")
-private Fuel fuel;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "fuel")
+    private Fuel fuel;
 
-@ManyToOne
-@JsonBackReference // use when we could not write JSON: Infinite recursion exception.
-@JoinColumn(name = "dealer_id", nullable = false)
-private Dealer dealer;
+    @ManyToOne
+    @JsonBackReference // use when we could not write JSON: Infinite recursion exception.
+    @JoinColumn(name = "dealer_id", nullable = false)
+    private Dealer dealer;
 
 
-public enum Model {
-SUV, SPORTS_CAR, COUPE, MINIVAN, CONVERTIBLE, HATCHBACK, SEDAN, PICKUP_TRUCK
-}
+    public enum Model {
+        SUV, SPORTS_CAR, COUPE, MINIVAN, CONVERTIBLE, HATCHBACK, SEDAN, PICKUP_TRUCK
+    }
 
-public enum Gear {
-AUTOMATIC, MANUAL
-}
+    public enum Gear {
+        Automatic, Manual
+    }
 
-public enum Fuel {
-DIESEL, PETROL
-}
+    public enum Fuel {
+        Diesel, Petrol
+    }
 }
--- src/test/java/com/CarRentalAgency/CarRentalAgencyApplicationTests.java
+++ src/test/java/com/CarRentalAgency/CarRentalAgencyApplicationTests.java
@@ -1,27 +1,13 @@
 package com.CarRentalAgency;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.ResponseEntity;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 class CarRentalAgencyApplicationTests {
 
-    @LocalServerPort
-    private int port;
+	@Test
+	void contextLoads() {
+	}
 
-    @Autowired
-    private TestRestTemplate restTemplate;
-
-    @Test
-    void contextLoads() {
-        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:" + port + "/", String.class);
-        assertThat(response.getStatusCode()).isEqualTo(200);
-    }
 }
