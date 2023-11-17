a5a9372eb01355a351df00524f00751a7d87664c
Alperen Dagli <alperen.dagli43@gmail.com>
2023-11-17 22:06:39 +0300

    ai generated changes cycle 1

--- src/test/java/com/CarRentalAgency/CarRentalAgencyApplicationTests.java
+++ src/test/java/com/CarRentalAgency/CarRentalAgencyApplicationTests.java
@@ -1,13 +1,27 @@
 package com.CarRentalAgency;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class CarRentalAgencyApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @LocalServerPort
+    private int port;
 
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void contextLoads() {
+        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:" + port + "/", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(200);
+    }
 }
